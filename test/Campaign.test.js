575b9c1601f4dbe5809458c524d8cf8324649a6f
Tobi Adegbuji <adegbuji_tobi@yahoo.com>
2022-02-14 16:07:05 -0500

    Fixed Test

--- test/Campaign.test.js
+++ test/Campaign.test.js
@@ -15,18 +15,17 @@ let campaignAddress;
 beforeEach(async () => {
   accounts = await web3.eth.getAccounts();
 
-  factory = await new web3.eth.Contract(JSON.parse(compiledFactory.abi))
-    .deploy({
-      data: compiledFactory.evm.bytecode.object,
-    })
+  factory = await new web3.eth.Contract(compiledFactory.abi)
+    .deploy({data: compiledFactory.evm.bytecode.object}) 
     .send({
       from: accounts[0],
-      gas: "1000000",
+      gas: "3000000",
     });
+
 
     await factory.methods.createCampaign('100').send({
       from: accounts[0],
-      gas: "1000000"
+      gas: "3000000"
     });
     
     //This means we are taking the first element from the array and assigning it to the campaignAddress variable above. 
@@ -34,9 +33,17 @@ beforeEach(async () => {
 
     //Notice we are not deploying anything here. 
     campaign = await new web3.eth.Contract(
-        JSON.parse(compiledCampaign.abi),
+        compiledCampaign.abi,
         campaignAddress
-    ); 
-    
+    );   
 
 });
+
+
+
+describe('Campaigns', () => {
+  it('deploys a factory and a campaign', () => {
+    assert.ok(factory.options.address);
+    assert.ok(campaign.options.address); 
+  })
+})
